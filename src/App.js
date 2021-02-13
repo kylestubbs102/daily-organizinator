59177743decfb9e4e3af17670ee6f39a00c34a52
ryanlavu <ryanvu@ucsb.edu>
2021-02-12 22:00:46 -0800

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import './App.css';
 import Form from './components/Form';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import DayOrganizer from './components/DayOrganizer';
+import Login from './components/Login'
+import Logout from  './components/Logout'
 
 class App extends Component {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [name, setName] = useState()
+
   render(){
+    
     return (
         <div className="App">
             <Form />
             <DayOrganizer />
         </div>
       )
+      
   }
 }
 
