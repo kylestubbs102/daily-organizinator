e8fd0dfd56a958c23707f602f80cbc558fdd9353
ryanlavu <ryanvu@ucsb.edu>
2021-02-12 22:15:12 -0800

    asdasd

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+import { GoogleLogin } from 'react-google-login';
+// refresh token
+import { refreshTokenSetup } from '../utils/refreshToken';
+
+const clientId = '249253331405-95j048peh8kle948lgfdi09n4rn2sjpq.apps.googleusercontent.com' //insert client id here
+
+function Login(props) {
+
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully! Welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+    refreshTokenSetup(res);
+    props.setLoggedIn(true);
+    props.setName(res.profileObj.name)
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login 😢. Please try again`
+    );
+  };
+
+  
+  return (
+
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Logout.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { GoogleLogout } from 'react-google-login';
+
+const clientId = '249253331405-95j048peh8kle948lgfdi09n4rn2sjpq.apps.googleusercontent.com' //insert client id here
+
+function Logout(props) {
+
+  const onSuccess = () => {
+    console.log('Logout made successfully');
+    alert('Logout made successfully ✌');
+    props.setLoggedIn(false);
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
+}
+
+export default Logout;
