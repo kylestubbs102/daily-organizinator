4b6e003859f45895358ba1c977ffb2e0491ec3a8
tnachian70 <77037215+tnachian70@users.noreply.github.com>
2021-02-20 18:19:28 -0800

    app popup form component

---
+++ src/Modal.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Form, Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Form, Button, Modal } from "react-bootstrap";
+import ReactDOM from 'react-dom';
+
+<Form>
+    <Form.Group controlId="formBasicEmail">
+        <Form.Label>Description</Form.Label>
+        <Form.Control placeholder="Enter Description" />
+    </Form.Group>
+  
+    <Form.Group controlId="formBasicPassword">
+        <Form.Label>Day</Form.Label>
+        <Form.Control  placeholder="Enter day" />
+    </Form.Group>
+    <Button variant="primary" type="submit">Submit</Button>
+</Form>  
+  
